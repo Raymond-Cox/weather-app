ef72408528d6a01c19c84697e691064eb24f00e2
Raymond Cox <raymondc1911@gmail.com>
2018-05-26 09:49:38 -0400

    short api replace

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const Header = ({ title, subtitle }) => {
+const Header = ({ title, subtitle, credits }) => {
   console.log(title, subtitle);
   return (
     <div>
       <h1>{title}</h1>
       <h2>{subtitle}</h2>
+      <h2><a href="https://darksky.net/poweredby/">{credits}</a></h2>
     </div>
   );
 
@@ -14,6 +15,7 @@ const Header = ({ title, subtitle }) => {
 Header.defaultProps = {
   title: 'Weather App',
   subtitle: 'Serving the weather in your local area',
+  credits: 'Powered by Dark Sky',
 }
 
 export default Header;
--- src/ReturnData.js
+++ src/ReturnData.js
@@ -12,21 +12,22 @@ class ReturnData extends Component {
 
   getWeatherData() {
     console.log('this is from getWeatherData');
-    fetch('http://api.openweathermap.org/data/2.5/forecast?zip=45679,us&units=imperial&APPID=c30fb0dcacdbff8a6a6331b5b4552e2a')
+    // fetch('http://api.openweathermap.org/data/2.5/forecast?zip=45679,us&units=imperial&APPID=c30fb0dcacdbff8a6a6331b5b4552e2a')
+    fetch('https://api.darksky.net/forecast/7685c4ab164d2cf45c58d6f908d7635a/37.8267,-122.4233', {'mode': 'no-cors' })
       .then(data => data.json())
       .then(results => {
         console.log(results);
-        let weatherData = [];
-        const location = `${results.city.name}, ${results.city.country}`;
+        // let weatherData = [];
+        // const location = `${results.city.name}, ${results.city.country}`;
 
         /** Map through data list and pull out every 8 (3pm each day) */
-        for (let i = 5; i < results.list.length; i += 8) {
-          weatherData.push(results.list[i]);
-        }
-        this.setState({
-          weatherData: weatherData,
-          location: location,
-        });
+        // for (let i = 5; i < results.list.length; i += 8) {
+        //   weatherData.push(results.list[i]);
+        // }
+        // this.setState({
+        //   weatherData: weatherData,
+        //   location: location,
+        // });
       })
       .catch(error => {
         console.log('An error occurred: ', error);
@@ -39,10 +40,18 @@ class ReturnData extends Component {
 
   render() {
     console.log(this.state.weatherData);
-    return (
-      <div>
-        <h3>{this.state.location}</h3>
-        {this.state.weatherData && this.state.weatherData.map((day, index) => {
+    
+    {/*  return (
+      <div> 
+      </div>
+      );
+      */}
+    
+
+      return (
+        <div>
+      {/* <h3>{this.state.location}</h3>
+          {this.state.weatherData && this.state.weatherData.map((day, index) => {
           const weekDay = new Date(day.dt_txt);
           const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
           const dayName = days[weekDay.getDay()];
@@ -56,10 +65,12 @@ class ReturnData extends Component {
               <hr />
             </div>
           )}
-        )}
+          )} */}
       </div>
     );
-  }
+  } 
+
+
 
 }
 
